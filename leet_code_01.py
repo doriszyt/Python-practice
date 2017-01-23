65b91923080d8828c043621e17ac115eb4df5581
doris <doris@51.ca>
2017-01-23 11:57:02 -0500

    leetcode testing

--- leet_code_01.py
+++ leet_code_01.py
@@ -3,22 +3,23 @@ class Solution(object):
 
         biX = bin(x)[2:]
         biY = bin(y)[2:]
-        ll = max(len(biX),len(biY))
-        sl = min(len(biX),len(biY))
-        if len(biX)<=len(biY):
-            biX = '0'*(ll-sl) + biX
+        ll = max(len(biX), len(biY))
+        sl = min(len(biX), len(biY))
+        if len(biX) <= len(biY):
+            biX = '0' * (ll - sl) + biX
         else:
-            biY = '0'*(ll-sl) + biY
+            biY = '0' * (ll - sl) + biY
         n = 0
         for i in xrange(ll):
             if biX[i] != biY[i]:
                 n += 1
         return n
 
-    print hammingDistance(1,1,17)
+    print hammingDistance(1, 1, 17)
+
 
 class solution283(object):
-    def moveZeroes(self,nums):
+    def moveZeroes(self, nums):
         index = 0
         size = len(nums)
 
@@ -27,20 +28,18 @@ class solution283(object):
                 continue
             else:
                 nums[index] = nums[i]
-                index += 1    #index = index+1
+                index += 1  # index = index+1
 
-        while index<size:
+        while index < size:
             nums[index] = 0
-            index +=1
+            index += 1
 
         return
+
 
 class Solution1(object):
     def twoSum(self, nums, target):
         index = 0
-        
-
-
 
         return
 
