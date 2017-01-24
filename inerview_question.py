efc5b52f910b53dcff0aa3c82a85b89d5907d6ef
doris <doris@51.ca>
2017-01-24 09:20:06 -0500

    leetcode testing

--- inerview_question.py
+++ inerview_question.py
@@ -1,7 +1,7 @@
-class interview(object):
+
     #Write a function that prints the numbers from 1 to 100.
 
-    def question_1(self):
+def question_1():
         number = range(1,100)
         for i in number:
             if i % 3 == 0:
@@ -12,3 +12,16 @@ class interview(object):
                 print i
         return
 
+def question_2(int):
+    temp = 0
+    for i in range(0,int+1):
+        if i % 2 == 0:
+            temp +=1
+    print temp
+    return
+
+def question_3():
+
+    return
+
+print question_1()
